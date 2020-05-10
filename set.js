bbd78a4a9fd8fae3d5d992f6bed2961e943e207b
Mauricio Yochizumi <mauricioyochizumi@MacBook-Pro-de-Mauricio-7.local>
2020-05-09 22:21:11 -0300

    set: union method

--- set.js
+++ set.js
@@ -68,26 +68,78 @@ function Set() {
     }
     return values;
   }
+
+  /**
+   * Set operations
+   * 1. union
+   * 2. intersection
+   * 3. difference
+   * 4. subset
+   */
+
+   /**
+    * Function: Union
+    */
+  this.union = function(otherSet) {
+    let unionSet = new Set();
+    let values = this.values(); // [elem1, elem2, ...]
+
+    for( let i = 0 ; i < values.length ; i++ ) {
+      unionSet.add(values[i]);
+    }
+    values = otherSet.values();
+
+    for(let i = 0 ; i < values.length ; i++) {
+      unionSet.add(values[i]);
+    }
+
+    return unionSet;
+  }
+
 }
 
 
 /**
  * Example usage
  */
-let set = new Set();
+// let set = new Set();
 
-set.add(1);
-set.add(2);
-set.add(3);
-set.add(4);
+// set.add(1);
+// set.add(2);
+// set.add(3);
+// set.add(4);
 
-console.log(set.values());
-console.log(set.size());
+// console.log(set.values());
+// console.log(set.size());
 
 
-console.log('deleting value 4');
-set.delete(4);
-console.log(set.values());
-console.log(set.size());
-console.log(set.has(2));
-console.log(set.has(4));
+// console.log('deleting value 4');
+// set.delete(4);
+// console.log(set.values());
+// console.log(set.size());
+// console.log(set.has(2));
+// console.log(set.has(4));
+
+
+
+
+
+
+/**
+ * Union example
+ */
+
+var setA = new Set();
+setA.add(1);
+setA.add(2);
+setA.add(3);
+
+var setB = new Set();
+setB.add(3);
+setB.add(4);
+setB.add(5);
+setB.add(6);
+
+
+var unionAB = setA.union(setB);
+console.log(unionAB.values());
